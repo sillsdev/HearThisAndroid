215972ca6361071ce738b42bedbb19574c002c6d
John Thomson <john_thomson@sil.org>
2014-10-01 01:10:21 -0500

    Record and Play buttons are working

--- Activities/org/sil/hearthis/RecordActivity.java
+++ Activities/org/sil/hearthis/RecordActivity.java
@@ -1,5 +1,8 @@
 package org.sil.hearthis;
 
+import java.io.File;
+import java.io.IOException;
+
 import Script.BookInfo;
 import Script.IScriptProvider;
 import Script.Project;
@@ -8,10 +11,19 @@ import Script.ScriptLine;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.media.MediaRecorder.AudioEncoder;
+import android.media.MediaRecorder.AudioSource;
+import android.media.MediaRecorder.OutputFormat;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ScrollView;
@@ -22,6 +34,9 @@ public class RecordActivity extends Activity {
 	int _activeLine;
 	ViewGroup _linesView;
 	int _lineCount;
+	
+	MediaRecorder recorder = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -56,6 +71,26 @@ public class RecordActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				nextButtonClicked();
+			}
+		});
+		
+		Button record =  (Button) findViewById(R.id.recordButton);
+		record.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent e) {
+				recordButtonTouch(e);
+				return true; // we handle all touch events on this button.
+			}
+
+		});
+		
+		Button play =  (Button) findViewById(R.id.playButton);
+		play.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				playButtonClicked();
 			}
 		});
 	}
@@ -103,5 +138,83 @@ public class RecordActivity extends Activity {
 			}
 		}
 		scrollView.scrollTo(0, scrollPos);
+	}
+	
+	void recordButtonTouch(MotionEvent e) {
+		int maskedAction = e.getActionMasked();
+
+	    switch (maskedAction) {
+		    case MotionEvent.ACTION_DOWN: {
+		    	startRecording();
+		      break;
+		    }
+		    case MotionEvent.ACTION_UP:
+		    case MotionEvent.ACTION_CANCEL: {
+		    	stopRecording();
+		      break;
+		    }
+	    }
+	}
+	
+	String fileName = "TheOneHearThisFile.mpg4";
+	
+	void startRecording() {
+		if (recorder != null) {
+	      recorder.release();
+		}
+		recorder = new MediaRecorder();
+		recorder.setAudioSource(AudioSource.MIC);
+		// Looking for a good combination that produces a useable file.
+		// THREE_GPP/AMR_NB was suggested at http://www.grokkingandroid.com/recording-audio-using-androids-mediarecorder-framework/
+		// Eclipse complains that AMR_NB not supported in API 8 (requires 10).
+		// http://www.techotopia.com/index.php/Android_Audio_Recording_and_Playback_using_MediaPlayer_and_MediaRecorder
+		// also suggests THREE_GPP/AMR_NB. In another place they suggest AAC_ADTS/AAC.
+		// THREE_GPP/AMR_NB produces a small file which neither phone nor WMP can play.
+		// THREE_GPP/DEFAULT likewise.
+		// This combination produces a file that WMP can play.
+		recorder.setOutputFormat(OutputFormat.MPEG_4);
+		recorder.setAudioEncoder(AudioEncoder.AAC);
+		// Todo:  file name and location based on book, chapter, segment
+		File file = new File(getExternalFilesDir(null), fileName);
+		recorder.setOutputFile(file.getAbsolutePath());
+		try {
+			recorder.prepare();
+			recorder.start();
+		} catch (IOException e) {
+	//			      Log.e("giftlist", "io problems while preparing [" +
+	//			            file.getAbsolutePath() + "]: " + e.getMessage());
+		}		
+	}
+	
+	void stopRecording() {
+	   if (recorder != null) {
+		   recorder.stop();
+		   recorder.reset();
+		   recorder.release();
+		   File file = new File(getExternalFilesDir(null), fileName);
+		   Log.d("Recorder", "Recorder finished and made file " + file.getAbsolutePath() + " with length " + file.length());
+		   recorder = null;
+	   }
+	}
+	
+	void playButtonClicked() {
+		MediaPlayer mp = new MediaPlayer();
+		try {
+			// Todo:  file name and location based on book, chapter, segment
+
+//			AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+//			int maxVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+//			Log.d("Player", "current volume is " + audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+//					+ " of max " + maxVol);
+//			audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVol, 0);
+			
+			File file = new File(getExternalFilesDir(null), fileName);
+			mp.setDataSource(file.getAbsolutePath());
+			mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+			mp.prepare();
+			mp.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
 	}
 }
